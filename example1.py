79deee77ab01c9d155ff8920bd146e8e5a501e16
Darya <darya.rempel@bk.ru>
2022-12-02 17:15:12 +0600

    Задания

--- example1.py
+++ example1.py
@@ -1,45 +1,45 @@
-# #Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
-# string = input('Введите вещественное число: ').split('.')
-# a = string[1]
-# b = string[0]
-# a = int(a)
-# b = int(b)
-# n = a
-# d = 1
-# m = b
-# y = 1
-# sumleftpart = 0
-# sumrightpart = 0
-# while n > 0:
-#     d  = n % 10
-#     n = n // 10
-#     sumleftpart += d
+string = input('Введите вещественное число: ').split('.')
+a = string[1]
+b = string[0]
+a = int(a)
+b = int(b)
+n = a
+d = 1
+m = b
+y = 1
+sumleftpart = 0
+sumrightpart = 0
+while n > 0:
+    d  = n % 10
+    n = n // 10
+    sumleftpart += d
     
-# while m > 0:
-#     y = m % 10
-#     m = m // 10
-#     sumrightpart += y
+while m > 0:
+    y = m % 10
+    m = m // 10
+    sumrightpart += y
     
 
-# print(sumleftpart + sumrightpart)
+print(sumleftpart + sumrightpart)
  
 
 
  
-# #Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
  
-# n = int(input('Ведите число: N = '))
+n = int(input('Ведите число: N = '))
 
-# result = 1
-# list = []
-# if n > 0:
-#     for i in range(1, n+1):
-#          result = i * result
-#          list.append(result)
+result = 1
+list = []
+if n > 0:
+    for i in range(1, n+1):
+         result = i * result
+         list.append(result)
 
-#     print ('Набор произведений чисел от 1 до {} = {}' .format(n, list))
-# else: print('Число не может быть отрицательным и не должно быть равно нулю')
+    print ('Набор произведений чисел от 1 до {} = {}' .format(n, list))
+else: print('Число не может быть отрицательным и не должно быть равно нулю')
 
 
 # Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
@@ -74,31 +74,31 @@ print('{}  Сумма {} '.format(dictionary,summ))
 # -3 * -1 * 0 = 0
 # Вывод: 0
 
-# n = int(input('Ведите число: N = '))
-# list = []
-# result = 1
-# for m in range (-n,n+1):
-#     list.append(m)
+n = int(input('Ведите число: N = '))
+list = []
+result = 1
+for m in range (-n,n+1):
+    list.append(m)
 
-# a = []
-# for element in input('Введите индексы через пробел: ').split():
-#      a.append(int(element))
+a = []
+for element in input('Введите индексы через пробел: ').split():
+     a.append(int(element))
  
 
-# for k in (a): 
-#      j = k
-#      i = list[j]
-#      result = i * result
+for k in (a): 
+     j = k
+     i = list[j]
+     result = i * result
        
-# print('Список : {}'.format(list))
-# print('Индексы элементов: {}'.format(a))
-# print('Произведение элементов =  {}'.format(result))
+print('Список : {}'.format(list))
+print('Индексы элементов: {}'.format(a))
+print('Произведение элементов =  {}'.format(result))
 
-# # Реализуйте алгоритм перемешивания списка.
-# import random
-# list = ['1', '2', '3', '4', '5'] 
-# random.shuffle(list) 
-# print(list)
+# Реализуйте алгоритм перемешивания списка.
+import random
+list = ['1', '2', '3', '4', '5'] 
+random.shuffle(list) 
+print(list)
 
 
 
